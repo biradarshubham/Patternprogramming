0c22886f144d45b3ca406611c7238236cf4ddd22
shubham biaradar <159401719+biradarshubham@users.noreply.github.com>
2024-07-15 21:08:59 +0530

    Add files via upload

---
+++ Pattern25.java
@@ -0,0 +1,32 @@
+package Patternprograms;
+
+/*
+ 
+* * * *       
+  * * * *     
+    * * * *   
+      * * * *  
+ 
+ */
+public class Pattern25 {
+
+	public static void main(String[] args) {
+		int n=4;
+		for(int i=1;i<=n;i++) 
+		{
+			for(int j=1;j<n*2;j++) //n*2
+			{
+				if( j-i<n && i<=j)
+				{                                                                                                                   
+					System.out.print("* ");
+				}else 
+				{
+					System.out.print("  ");
+				}
+			}
+		System.out.println();
+
+	}
+	}
+
+}
